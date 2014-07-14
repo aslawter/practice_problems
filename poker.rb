9554fd4af5eacc6aae32b995b06f38ce4e53372c
Andrew Slawter <aslawter@gmail.com>
2014-07-14 14:54:22 -0400

    Initialize Deck with cards

--- poker.rb
+++ poker.rb
@@ -1,45 +1,59 @@
 class Poker
   def initialize(deck)
-    @deck = Deck.new
+    @deck = deck
   end
 
   def play
     select_number_of_players
-    deal_cards
     create_hands
+    deal
     print_hands
   end
 
   def select_number_of_players
-    puts "Enter the number of players for this game" 
+    puts "Enter the number of players for this game"
     print ">"
     number_of_players = gets.chomp
-  end 
+  end
 
-  def create_hands
-    @players.each do |player|
-      player.deal
-    end
-    
+ # def create_hands
+ #   @players.each do |player|
+ #     player.deal
+ #   end
+ #  end
+
+  1.upto(@players) do |hands|
+    hands
+  end
+
   end
 
 end
 
 class Deck
-  def initialize(cards)
-    @cards = cards.shuffle
+  def initialize(@cards)
+    @deck = cards.shuffle
   end
 
-  cards = {"hearts" => [array_of_hearts], "spades" => [array_of_spades], "diamonds" => [array_of_diamonds], "clubs" => [array_of_clubs]}
-  suites = ["hearts", "spades", "diamonds", "clubs"]
-  
+class Card
+  def initialize 
+    suites = ["Hearts", "Spades", "Diamonds", "Clubs"]
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
 
-  class Player
-    def initialize(number_of_players)
-      @players = Players.all
-
+    @cards = []
+    suits.each do |i|
+      ranks.length.times do |i|
+        @cards.push(Card.new(ranks[i], suit, (i + 1)))
+      end
     end
   end
+end
 
-  poker = Poker.new(deck)
-  poker.play
+class Player
+  def initialize(number_of_players)
+      @players = number_of_players
+  end
+end
+
+poker = Poker.new(deck)
+poker.play
