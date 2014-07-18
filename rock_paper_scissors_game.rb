d71aacf3785f6f06f7b5852353e55e9e41e8eee4
Andrew Slawter <aslawter@gmail.com>
2014-07-18 13:01:51 -0400

    Refactor to count rounds
    *Count rounds
    *Display current round

--- rock_paper_scissors_game.rb
+++ rock_paper_scissors_game.rb
@@ -1,15 +1,22 @@
 require "./round"
 
 class RockPaperScissorsGame
+
+  STARTING_ROUND = 1
+
   def initialize(round)
     @round = round
   end
 
   def play
+    current_round = STARTING_ROUND
     loop do
+      puts "Round: #{current_round}"
       @round.game
+      current_round = current_round + 1
     end
   end
+
 end
 
 round = Round.new
